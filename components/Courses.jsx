e398bcc1d4ea778355e3609e0064b9c25e7ef01a
KMG <rlaaudrb1104@gmail.com>
2023-09-26 11:43:34 +0900

    수업최종본

--- components/Courses.jsx
+++ components/Courses.jsx
@@ -1,14 +1,12 @@
 import Link from "next/link";
 
-async function fetchCourses() {
-  const response = await fetch("http://localhost:3000/api/courses");
-  const courses = response.json();
-  return courses;
-}
+//async function fetchCourses() {
+//const response = await fetch("http://localhost:3000/api/courses");
+//const courses = response.json();
+//return courses;
+//}
 
-const Courses = async () => {
-  const courses = await fetchCourses();
-
+const Courses = async ({ courses }) => {
   return (
     <div className="grid-1 ">
       {courses?.map((course) => (
